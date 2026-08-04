4468deb25eac91271e1be6988258238d29935616
Gabriel0-cmd <gabrielzardetto@colegiopaulodetarso.com.br>
2026-08-04 11:25:19 +0000

    opa

--- app.py
+++ app.py
@@ -8,18 +8,22 @@ def index():
         "nome": "Pedro",
         "turma": "2°EM Tecnico"
     }
-    professores =[
+    professores = [
         {
             "nome": "Ishara",
             "materia": "Web"
         },
-            {
-                "nome": "Edidio",
-                "materia": "Software"
-            }
+        {
+            "nome": "Edidio",
+            "materia": "Software"
+        }
     ]
-    return render_template('index.html', title="Home", aluno=aluno)
+    return render_template('index.html', title="Home", aluno=aluno, professores=professores)
 
 @app.route("/boletim")
 def boletim():
     return render_template('boletim.html', title="Boletim")
+
+@app.route("/sobremim")
+def sobremim():
+    return render_template('sobremim.html', title="Sobre mim")
